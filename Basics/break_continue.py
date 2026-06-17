57eff26b81778ae18aae2491054f5ecc1123532a
Aryan <your.email@example.com>
2026-06-17 11:34:23 +0530

    Edited leap year checker and created a new file break_continue ,py

---
+++ Basics/break_continue.py
@@ -0,0 +1,4 @@
+for i in range(12):
+    if (i == 10):
+        break
+    print("29 X", i+1,"=", 29 * (i+1))
--- Mini_Projects/Leapyear_Checker.py
+++ Mini_Projects/Leapyear_Checker.py
@@ -2,17 +2,17 @@ year = 2000
 year1 = 2023
 year2 = 2024
 
-if (year %4, year % 100, year%400):
+if (year %4 ==0, year %100 != 0, year %400 == 0):
     print("leap year")
 else:
     print("not a leap year")
 
-    if (year1 %4, year1 % 100, year1 %400):
-        print("Leap year")
-    else:
-        print("not leap year")
+if (year1 %4 ==0, year1 % 100 != 0, year1 %400 == 0):
+     print("Leap year")
+else:
+    print("not leap year")
 
-        if (year2 %4, year2 % 100, year2 %400):
-            print("Leap year")
-        else:
-            print("not leap year")
+if (year2 %4 == 0, year2 % 100 != 0, year2 %400 == 0):
+      print("Leap year")
+else:
+     print("not leap year")
