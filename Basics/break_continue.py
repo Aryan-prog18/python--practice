424696ec033fa95f32274b8f39cc9a7b3d64149d
Aryan <your.email@example.com>
2026-06-18 11:12:46 +0530

    Used break and continue function to make an multiplication table

--- Basics/break_continue.py
+++ Basics/break_continue.py
@@ -1,4 +1,14 @@
-for i in range(12):
-    if (i == 10):
-        break
-    print("29 X", i+1,"=", 29 * (i+1))
+while True:
+    num = int(input("Enter The Number: "))
+    if (num <= 0):
+        print("Invalid Input, Try again")
+        continue # if user enter invalid num so it will go back to the while loop and start again
+    for i in range(11):
+        print(num, "x", i, "=", num *(i))
+        if (i == 10):
+            break
+
+#! Here I used combination of functions to make this table generator
+# !-- Used while loop
+# !-- Used for loop 
+# !-- Used break and continue function
